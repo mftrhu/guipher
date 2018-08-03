6786fe96e7d1f625f28e48ae16cfb28b482b6c8b
mftrhu <mftrhu+github@inventati.org>
2018-08-03 12:59:45 +0000

    Handle URIs with protocol dispatch table, modify order of gopher quadruplets

--- gopher.scm
+++ gopher.scm
@@ -1,8 +1,61 @@
 #!/usr/bin/env guile
 ;; Guile + PS/Tk Gopher client !#
 
-(use-modules (ice-9 rdelim) (srfi srfi-1) (web uri))
+(use-modules (ice-9 rdelim) (ice-9 regex) (srfi srfi-1) (web uri))
 (load "pstk.scm")
+
+(declare-default-port! "gopher" 70)
+(define default-protocol "gopher://")
+
+(define (gopher-handler uri)
+  (debug "Gopher jump-to ~a" (uri-to-quadruplet uri))
+  (gopher-goto (uri-to-quadruplet uri)))
+
+(define (about-handler uri)
+  (debug "About jump-to ~a" (uri-path uri))
+  (cond
+   [(equal? (uri-path uri) "blank")
+    (debug "YOYOYOYOYO")
+    (main-text 'config 'state: 'normal)
+    (main-text 'delete '1.0 'end)
+    (main-text 'config 'state: 'disabled)]))
+
+(define protocol-handlers
+  (list
+   (cons 'gopher gopher-handler)
+   (cons 'about about-handler)))
+
+(define (default-handler uri)
+  (debug "Unknown protocol ~a\n" (uri-scheme uri)))
+
+(define (dispatch-by-uri string)
+  (let [(parsed (string->uri string))]
+    (if (not parsed)
+        ;; Not parsed, likely missing the protocol - add it and try
+        ;; again, *once*, throwing an error on the second attempt
+        (if (not (string-prefix? default-protocol string))
+            (dispatch-by-uri (string-append default-protocol string))
+            (display "Malformed URI\n"))
+        ;; Handle the URI with the proper function
+        (let [(handler (assoc (uri-scheme parsed) protocol-handlers))]
+          (if handler
+              ((cdr handler) parsed)
+              (default-handler parsed))))))
+
+(define (uri-to-quadruplet uri)
+  (let [(host (uri-host uri))
+        (port (or (uri-port uri) 70))
+        (path (uri-path uri))]
+    ;; Check to see if there is a possible selector in the path
+    (if (string-match "/[0-9a-zA-Z]/" path)
+        ;; There is - extract and remove it from the path
+        (let [(path (substring path 3))
+              (selector (substring path 1 2))]
+          (list selector host port path))
+        ;; There is not - assume we are dealing with a menu
+        (list "1" host port path))))
+
+;;====================================================================;;
 
 (define (debug message . rest)
   (apply format #t (string-concatenate (list "DEBUG: " message "\n")) rest))
@@ -27,8 +80,9 @@
 (define (tk-get-list name)
   (string-split (tk-get-var name) #\space))
 
-(define (gopher-get address selector port)
+(define (gopher-get address port selector)
   "Returns a list of lines containing the server's response to a Gopher request at ADDRESS:PORT for SELECTOR."
+  (debug "gopher-get recived (~a ~a ~a)" address port selector)
   (let [(s (socket PF_INET SOCK_STREAM 0))]
     (connect s AF_INET (get-ip-of address) port)
     (display (format #f "~a\r\n" selector) s) ; Ask for the selector
@@ -43,6 +97,15 @@
 ;; Stacks for the history
 (define hist-back '())
 (define hist-forw '())
+;; Item type -> tag - icon - handler map
+(define gopher-types
+  '(("0" . ("text" "img-text" gopher-render-text))
+    ("1" . ("directory" "img-directory" gopher-render-menu))
+    ("7" . ("search" "img-search" gopher-render-menu))
+    ("g" . ("image" "img-image" gopher-render-image))
+    ("I" . ("image" "img-image" gopher-render-image))
+    ("9" . ("binary" "img-binary" gopher-download))))
+
 
 (define (gopher-history-back)
   (if (>= (length hist-back) 2)
@@ -78,7 +141,7 @@
       (set! line-tags '("no-icon"))])
     (if (> (string-length selector) 0)
         (let [(tag (format #f "LINK:~a:~a:~a" host port selector))]
-          (hash-set! links tag (list kind host selector port))
+          (hash-set! links tag (list kind host port selector))
           (set! line-tags (append line-tags (list tag)))))
     (widget 'insert 'end
             (string-concatenate (list " " (list-ref pieces 0) "\n"))
@@ -114,7 +177,7 @@
    lines)
   (widget 'config 'state: 'disabled))
 
-(define (gopher-jump-to-address)
+(define (--gopher-jump-to-address)
   (debug "Jumping to ~a" (address-bar 'get))
   ;; TODO: this should actually check for the presence of a protocol, use
   ;;   `gopher://` if none is present, and show an error or dispatch to
@@ -135,6 +198,12 @@
     (gopher-goto (list "1" host selector port)))
   (tk/focus main-text))
 
+(define (gopher-jump-to-address)
+  (debug "Trying to jump to ~a" (address-bar 'get))
+  (let [(address (address-bar 'get))]
+    (dispatch-by-uri address))
+  (tk/focus dummy))
+
 (tk-start)
 ;; Initial window settings
 (tk/wm 'title tk "Guipher")
@@ -145,6 +214,8 @@
 (tk/image 'create 'photo "img-refresh" 'file: "assets/arrow_refresh.png")
 (tk/image 'create 'photo "img-directory" 'file: "assets/folder.png")
 (tk/image 'create 'photo "img-text" 'file: "assets/page.png")
+;; Dummy widget to receive focus
+(define dummy (tk 'create-widget 'frame))
 ;; Define the tool bar
 (define tool-bar
   (tk 'create-widget 'frame 'bd: 1 'relief: 'raised))
@@ -200,9 +271,9 @@
       (gopher-render-directory main-text (apply gopher-get triplet))
       (address-bar 'set (format #f "gopher://~a:~a/~a~a"
                                 (list-ref triplet 0)
-                                (list-ref triplet 2)
+                                (list-ref triplet 1)
                                 kind
-                                (list-ref triplet 1)))]
+                                (list-ref triplet 2)))]
      [(equal? kind "0")
       (gopher-render-text main-text (apply gopher-get triplet))]
      [else
@@ -222,6 +293,7 @@
              ,main-text %x %y))
 ;; Load the home page/address given on the command line
 ;; TODO: actually do it
-(gopher-goto '("1" "localhost" "/" 70))
+;;(gopher-goto '("1" "localhost" 70 "/"))
+(dispatch-by-uri "gopher://localhost/")
 ;; Start Tk
 (tk-event-loop)
